00b03a976b7130a057b48652f3b8685b4bd80b0f
Sven Wolf <sheklerwolf@outlook.de>
2025-07-06 21:09:50 +0200

    Add Pinia module and update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
 	compatibilityDate: "2025-05-15",
 	devtools: { enabled: true },
-	modules: ["@nuxt/image", "@nuxt/devtools", "@nuxt/eslint", "@nuxt/ui", "@nuxtjs/tailwindcss"],
+	modules: ["@nuxt/image", "@nuxt/devtools", "@nuxt/eslint", "@nuxt/ui", "@nuxtjs/tailwindcss", "@pinia/nuxt"],
 });
