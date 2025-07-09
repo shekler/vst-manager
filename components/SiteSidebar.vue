1e3b68da99da7a69a9bcc193451d97209d442f38
Sven Wolf <sheklerwolf@outlook.de>
2025-07-09 14:07:13 +0200

    Enhance theme colors and improve SiteSidebar styling
    - Added new color variables `--color-gold` and `--color-powder` to the theme CSS for better design flexibility.
    - Updated `SiteSidebar.vue` to improve text styling and spacing, enhancing overall UI clarity.
    - Increased gap sizes between navigation items and adjusted icon styling for a more polished appearance.

--- components/SiteSidebar.vue
+++ components/SiteSidebar.vue
@@ -1,19 +1,19 @@
 <template>
-	<aside class="p-12 flex flex-col gap-12 justify-between bg-jet text-white">
-		<section class="flex flex-col gap-12">
+	<aside class="p-12 flex flex-col gap-12 justify-between bg-jet text-powder text-lg">
+		<section class="flex flex-col gap-24">
 			<NuxtImg src="/plugin_manager.png" alt="Logo" class="w-fit h-fit max-h-12" />
 			<nav>
-				<ul class="flex flex-col gap-6">
-					<li class="flex items-center gap-2"><IconBooks /> Library</li>
-					<li class="flex items-center gap-2"><IconBell /> Updates</li>
-					<li class="flex items-center gap-2"><IconSearch /> Search</li>
+				<ul class="flex flex-col gap-10">
+					<li class="flex items-center gap-4"><IconBooks class="size-8 rounded-full bg-powder p-1 text-black" /> Library</li>
+					<li class="flex items-center gap-4"><IconBell class="size-8 rounded-full bg-powder p-1 text-black" /> Updates</li>
+					<li class="flex items-center gap-4"><IconSearch class="size-8 rounded-full bg-powder p-1 text-black" /> Search</li>
 				</ul>
 			</nav>
 		</section>
 		<nav>
-			<ul class="flex flex-col gap-6">
-				<li class="flex items-center gap-2"><IconUser /> User</li>
-				<li class="flex items-center gap-2"><IconSettings /> Settings</li>
+			<ul class="flex flex-col gap-10">
+				<li class="flex items-center gap-4"><IconUser class="size-8 rounded-full bg-powder p-1 text-black" /> User</li>
+				<li class="flex items-center gap-4"><IconSettings class="size-8 rounded-full bg-powder p-1 text-black" /> Settings</li>
 			</ul>
 		</nav>
 	</aside>
