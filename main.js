ece8e1a71baa3f8bb20aaa59550f5abd77e1c47e
Sven Wolf <sheklerwolf@outlook.de>
2025-07-20 13:55:41 +0200

    Add Electron Forge support and update dependencies
    - Integrated Electron Forge by adding necessary scripts to `package.json` for starting, packaging, and making the application.
    - Updated `package-lock.json` and `package.json` to include new dependencies related to Electron Forge, enhancing the build process.
    - Added `electron-squirrel-startup` for improved startup handling on Windows.
    - Updated TypeScript and other development dependencies to their latest versions for better compatibility and performance.

---
+++ main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require("electron");
+const { spawn } = require("child_process");
+const path = require("path");
+
+let mainWindow;
+let frontProcess;
+
+function isProduction() {
+  // Check if the app is packaged (production) or running in development
+  return app.isPackaged;
+}
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  const frontPath = getPath();
+  frontProcess = spawn("node", [frontPath], {
+    detached: true,
+    stdio: "inherit",
+  });
+  mainWindow.loadURL("http://localhost:3000");
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+function getPath() {
+  if (!isProduction()) {
+    return path.join(__dirname, "../", ".output", "server", "index.mjs");
+  } else {
+    return path.join(__dirname, "../../", ".output", "server", "index.mjs");
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
