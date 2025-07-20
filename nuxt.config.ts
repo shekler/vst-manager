f126f00f6219fed44383cba8ce9381f9ec3294f0
Sven Wolf <sheklerwolf@outlook.de>
2025-07-20 12:09:30 +0200

    Remove electron forge completely and rid off all non-essential packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
-  nitro: {
-    preset: "static",
-  },
   app: {
     baseURL: "./",
     head: {
@@ -13,28 +9,10 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: "en",
       },
-      meta: [
-        {
-          "http-equiv": "Content-Security-Policy",
-          content: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; connect-src 'self' http://localhost:* ws://localhost:*; font-src 'self' data:;",
-        },
-      ],
-      link: [
-        { rel: "icon", type: "image/png", href: "/favicon/favicon-96x96.png", sizes: "96x96" },
-        { rel: "icon", type: "image/svg+xml", href: "/favicon/favicon.svg" },
-        { rel: "shortcut icon", type: "image/x-icon", href: "/favicon/favicon.ico" },
-        { rel: "apple-touch-icon", href: "/favicon/apple-touch-icon.png", sizes: "180x180" },
-        { rel: "manifest", href: "/favicon/site.webmanifest" },
-      ],
     },
   },
   css: ["~/assets/css/main.css"],
   compatibilityDate: "2025-05-15",
-  devtools: { enabled: process.env.NODE_ENV === "development" },
+  devtools: { enabled: true },
   modules: ["@nuxt/image", "@nuxt/devtools", "@nuxtjs/tailwindcss"],
-  runtimeConfig: {
-    public: {
-      version: process.env.npm_package_version || "1.0.0",
-    },
-  },
 });
