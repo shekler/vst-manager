c17799ae0b55f32957fcc244fb8684fec32019bf
Sven Wolf <sheklerwolf@outlook.de>
2025-07-23 16:47:26 +0200

    Refactor main.js and update package dependencies
    - Simplified `main.js` by removing unnecessary variables and functions related to process spawning.
    - Adjusted the dimensions of the main window for a more compact layout.
    - Updated the `start` script in `package.json` to directly launch the Electron app.
    - Removed `electron-squirrel-startup` from dependencies in `package.json` for a cleaner setup.
    - Upgraded Electron version in `package.json` to `^37.2.4` for improved stability and features.
    - Cleaned up `package-lock.json` by removing outdated dependencies and ensuring compatibility.

--- main.js
+++ main.js
@@ -1,43 +1,15 @@
 const { app, BrowserWindow } = require("electron");
-const { spawn } = require("child_process");
-const path = require("path");
-
-let mainWindow;
-let frontProcess;
-
-function isProduction() {
-  // Check if the app is packaged (production) or running in development
-  return app.isPackaged;
-}
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
     webPreferences: {
       nodeIntegration: true,
     },
   });
 
-  const frontPath = getPath();
-  frontProcess = spawn("node", [frontPath], {
-    detached: true,
-    stdio: "inherit",
-  });
-  mainWindow.loadURL("http://localhost:3000");
-  mainWindow.on("closed", () => {
-    mainWindow = null;
-  });
+  win.loadURL("http://localhost:3000");
 }
 
-function getPath() {
-  if (!isProduction()) {
-    return path.join(__dirname, "../", ".output", "server", "index.mjs");
-  } else {
-    return path.join(__dirname, "../../", ".output", "server", "index.mjs");
-  }
-}
-
-app.whenReady().then(() => {
-  createWindow();
-});
+app.whenReady().then(createWindow);
